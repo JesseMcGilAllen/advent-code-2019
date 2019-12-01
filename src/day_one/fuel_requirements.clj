881d0479e1a030d988fc3187cf6cb77a99236ef6
Jesse McGil Allen <jesse@jessemcgilallen.com>
2019-12-01 16:57:51 -0600

    Finished part 1 of day one of Advent of Code

--- src/day_one/fuel_requirements.clj
+++ src/day_one/fuel_requirements.clj
@@ -4,3 +4,16 @@
   "calculates the fuel required for the module
    first divide the mass by 3, then round down and finally substract 2."
   (- (Math/floor (/ mass 3.0)) 2))
+
+(defn read-lines [file-path]
+ "reads file at file path and stores in a sequence."
+  (with-open [rdr (clojure.java.io/reader file-path)]
+    (doall  (line-seq rdr))))
+
+
+(defn calculate-total-mass-needed []
+  "Calculates the total mass required from all modules provided by the input file by calling calculate-fuel-requirements on each."
+  (reduce +
+          (map calculate-fuel-requirements
+               (map #(Integer/parseInt %)
+                    (read-lines "/Users/caissa/clojure/advent-code-2019/resources/input.txt")))))
