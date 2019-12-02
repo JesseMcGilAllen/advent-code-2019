49e6191445be94008d2bbdc20f3411b860f415de
Jesse McGil Allen <jesse@jessemcgilallen.com>
2019-12-01 18:03:45 -0600

    added two functions to account for part two

--- src/day_one/fuel_requirements.clj
+++ src/day_one/fuel_requirements.clj
@@ -2,8 +2,15 @@
 
 (defn calculate-fuel-requirements [mass]
   "calculates the fuel required for the module
-   first divide the mass by 3, then round down and finally substract 2."
+   first divide the mass by 3, then round down and finally subtract 2."
   (- (Math/floor (/ mass 3.0)) 2))
+
+(defn fuel-requirements-cascade [mass]
+  "calculates the fuel required for the module
+   first divide the mass by 3, then round down and finally subtract 2.
+   If there is any left over mass from the division then calculate the leftover
+   until less than 3."
+  (reduce + (take-while pos? (rest (iterate calculate-fuel-requirements mass)))))
 
 (defn read-lines [file-path]
  "reads file at file path and stores in a sequence."
@@ -15,5 +22,12 @@
   "Calculates the total mass required from all modules provided by the input file by calling calculate-fuel-requirements on each."
   (reduce +
           (map calculate-fuel-requirements
+               (map #(Integer/parseInt %)
+                    (read-lines "/Users/caissa/clojure/advent-code-2019/resources/input.txt")))))
+
+(defn calculate-total-mass-cascade []
+  "Calculates the total mass required from all modules provided by the input file by calling fuel-requirements-cascade on each."
+  (reduce +
+          (map fuel-requirements-cascade
                (map #(Integer/parseInt %)
                     (read-lines "/Users/caissa/clojure/advent-code-2019/resources/input.txt")))))
